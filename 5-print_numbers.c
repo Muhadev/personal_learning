b7b82011ca36650e2449dfab9a1655c1f036ed15
Muhadev <muhammedfayemi@gmail.com>
2023-09-01 07:02:41 -0700

    task is added

---
+++ 5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int nums;
+	for (nums = 0; nums < 10; nums++)
+	{
+		printf("%d", nums);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ 6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	char nums;
+	for (nums = 0; nums < 10; nums++)
+	{
+		putchar('0' + nums);
+	}
+	putchar('\n');
+	return (0);
+}
