4e1497b9fe1f3b05d12ca9050bbafcded6d96faa
Muhadev <muhammedfayemi@gmail.com>
2023-09-04 04:34:38 -0700

    task is fix

--- main.h
+++ main.h
@@ -17,4 +17,6 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 #endif /* MAIN_H */
